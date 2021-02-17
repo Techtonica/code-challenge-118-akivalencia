546721789b5f46f6dd99b6461470b07a41c2eaa7
akivalencia <adilene.valencias@gmail.com>
2021-02-17 10:16:26 -0800

    thinking through it

--- challenges/118.js
+++ challenges/118.js
@@ -6,26 +6,30 @@
 //
 //
 // ```
-//
-// class Parrot {
-//  // ...
-// }
-//
-// let polly = new Parrot("Polly");
-// polly.speak();
-// // returns "Polly want a cracker!"
-//
-// let benStiller = new Parrot("Ben");
-// benStiller.speak();
-// // returns "Ben want a cracker!"
-//
-// let anonymous = new Parrot();
-// anonymous.speak();
-// // returns "A nameless parrot want a cracker!"
-//
-//
-// ```
-//
+//code from challenge 116 
+
+class Parrot {
+    //default parameters 
+    constructor(name = 'A nameless parrot') {
+      this.name = name;
+      }
+    //method speak 
+     speak(){
+          return (this.name + " want a cracker!");
+        }
+      }
+  
+  let polly = new Parrot("Polly");
+  polly.speak();
+  
+  let anonymous = new Parrot();
+  
+  anonymous.speak();
+
+
+console.log(polly.speak())
+console.log(anonymous.speak())
+
 //
 // Update the class definition so the example code runs as shown.
 //
@@ -52,17 +56,13 @@
 // After invoking the speak function 3 times, on the 4th invocation, `ApprenticeParrot` should wake up and respond normally.
 
 
-class Parrot {
- // ...
-}
-
 class ApprenticeParrot extends Parrot {
  // ...
 }
 
-let polly = new Parrot("Polly");
-polly.speak();
-// returns "Polly want a cracker!"
+// let polly = new Parrot("Polly");
+// polly.speak();
+// // returns "Polly want a cracker!"
 
 
 // const me = new ApprenticeParrot("Your Name");
